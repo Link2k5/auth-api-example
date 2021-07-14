23e093fa9c03c9030f3bd8cd04ea7df726db69c0
Samuel Gomes <hellish.doom@gmail.com>
2021-07-13 22:26:24 -0300

    Implentado método logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Lang;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -38,5 +39,12 @@ class AuthController extends Controller
                         response()->json([
                            'error' => Lang::get('auth.failed')
                         ], 400);
+    }
+
+    public function logout()
+    {
+        Auth::guard('api')->logout();
+
+        return response()->noContent();
     }
 }
