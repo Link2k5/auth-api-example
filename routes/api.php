e95b6300af71d7422ec506b46da6409afdc9fb9b
Samuel Gomes <hellish.doom@gmail.com>
2021-07-13 22:58:26 -0300

    Definida rota para refresh token

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,18 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
+// Login
 Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+
+// Refresh
+Route::post('refresh', [AuthController::class, 'refresh'])->name('api.refresh');
+
+// Login Required Routes
 Route::middleware('auth:api')->group(function(){
+    // List all users
     Route::get('/users', [AuthController::class, 'index'])->name('api.users');
+
+    // Logout (Revoke Token)
     Route::get('/logout', [AuthController::class, 'logout'])->name('api.logout');
 });
